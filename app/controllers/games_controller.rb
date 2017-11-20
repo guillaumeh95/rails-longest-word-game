df414eac90f23c4b7918ea5eba59c7393137e90b
Guillaume Hercot <guillaume.hercot@gmail.com>
2017-11-20 16:57:35 +0100

    Finished the exercise

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,44 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+    @start_time = Time.now
+  end
+
+  def score
+    @word = params[:word].upcase
+    @letters = params[:grid]
+    @start_time = params[:time].to_datetime
+    if !included?(@word, @letters)
+      @result = "Sorry but #{@word} can't be built with #{@letters.gsub(" ", ", ")}."
+      @score = 0
+    elsif !valid_word?(@word)
+      @result = "Sorry but #{@word} does not seem to be an English word..."
+      @score = 0
+    else
+      @result = "Congratulations! #{@word} is a valid English word!"
+      @score = compute_score(@word, Time.now - @start_time)
+    end
+    session[:score].nil? ? session[:score] = @score : session[:score] += @score
+  end
+
+  private
+
+  def valid_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    html_file = open(url).read
+    html_doc = JSON.parse(html_file)
+    return html_doc["found"]
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : (attempt.size * (1.0 - time_taken / 60.0)).round(1)
+  end
+
+end
